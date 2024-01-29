d431fa20f5cd1d749b1fe799a4735b22192dd4aa
Josef Hrebec <josefhrebec02@gmail.com>
2024-01-30 00:30:06 +0100

    Add dates to movie names and polish errors

--- app/actions.tsx
+++ app/actions.tsx
@@ -2,7 +2,9 @@
 
 interface movie {
   original_title: string;
+  original_name: string;
   release_date: string;
+  first_air_date: string;
   overview: string;
   id: number;
   vote_average: number;
@@ -10,7 +12,7 @@ interface movie {
 
 export async function searchFetch(name: string) {
   const moviesJson = await fetch(
-    `https://api.themoviedb.org/3/search/multi?query=${name}`,
+    `https://api.themoviedb.org/3/search/movie?query=${name}`,
     {
       method: "GET",
       headers: {
@@ -19,8 +21,29 @@ export async function searchFetch(name: string) {
       },
     }
   );
-  const movies = await moviesJson.json();
-  return movies.results.slice(0, 4).sort((a: movie, b: movie) => {
+  const nestedMovies = await moviesJson.json();
+
+  const movies = nestedMovies.results;
+  console.log(movies);
+  const normalizedMovieNames = (movies as movie[]).map((movie) => {
+    if (movie.original_title !== undefined) {
+      return movie.original_title.normalize("NFC").toLocaleLowerCase();
+    }
+    return movie.original_name.normalize("NFC").toLocaleLowerCase();
+  });
+
+  if (new Set(normalizedMovieNames).size !== movies.length) {
+    (movies as movie[]).forEach((movie) => {
+      movie.original_title = `${movie.original_title} (${
+        movie.release_date
+          ? movie.release_date.split("-")[0]
+          : movie.first_air_date
+          ? movie.first_air_date.split("-")[0]
+          : "?"
+      })`;
+    });
+  }
+  return movies.slice(0, 5).sort((a: movie, b: movie) => {
     return b.vote_average - a.vote_average;
   });
 }
