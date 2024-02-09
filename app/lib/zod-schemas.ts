bc866ac3550d54e44bf2a909d2a5202930a766c7
Josef Hrebec <josefhrebec02@gmail.com>
2024-02-09 14:13:56 +0100

    Add refine to zod schema

--- app/lib/zod-schemas.ts
+++ app/lib/zod-schemas.ts
@@ -4,9 +4,12 @@ export const UserSchema = z.object({
   username: z
     .string()
     .min(4, "Username has to be at least 4 characters long.")
-    .max(31, "Username has to be at most 31 characters long."),
+    .max(31, "Username has to be at most 31 characters long.")
+    .refine((str) => str.toLowerCase() === str, {
+      message: "Username has to be in lower case.",
+    }),
   password: z
     .string()
     .min(6, "Password has to be at least 6 characters long.")
-    .max(255, "Password has to be at least 255 characters long."),
+    .max(255, "Password has to be at most 255 characters long."),
 });
