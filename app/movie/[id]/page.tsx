cd089e8b55340f5b86dc93ba0f0f2c43d30c1be6
Josef Hrebec <josefhrebec02@gmail.com>
2024-02-12 19:52:13 +0100

    Add favourite toggle

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,44 +1,27 @@
 import Image from "next/image";
-import Link from "next/link";
-import { shortenSummary } from "@/app/lib";
-import { movie } from "@/app/types";
-
-async function fetcher(id: string, similar: boolean = false) {
-  try {
-    const movieJson = await fetch(
-      `https://api.themoviedb.org/3/movie/${id}${similar ? "/similar" : ""}`,
-      {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          Authorization: `${process.env.API_TOKEN}`,
-        },
-      }
-    );
-    const movie = await movieJson.json();
-    if (similar) {
-      return movie.results
-        .sort((a: movie, b: movie) => {
-          return b.vote_average - a.vote_average;
-        })
-        .slice(0, 4);
-    }
-    return movie;
-  } catch (e) {
-    throw e;
-  }
-}
+import { Movie } from "@/app/lib/types";
+import { validateRequest } from "@/app/lib/auth";
+import { fetcher } from "@/app/actions/tmdb-actions";
+import { isFavourite } from "@/app/actions/favourite-actions";
+import SimilarMovies from "@/app/components/similar-movies";
+import Favourite from "@/app/components/favourite";
 
 export default async function Movie({ params }: { params: { id: string } }) {
-  const movie: movie = await fetcher(params.id);
-  const similarMovies: movie[] = await fetcher(params.id, true);
+  const movie: Movie = await fetcher(params.id);
+
+  const { user } = await validateRequest();
+  let isFav: boolean | undefined = false;
+
+  if (user) {
+    isFav = await isFavourite(params.id, user.id);
+  }
 
   return (
     <div className="flex flex-col gap-4 md:flex-row sm:justify-center">
       <div className="flex flex-col gap-4 sm:flex-row max-w-[1124px]">
         <div className="sm:flex-1">
           <Image
-            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
             alt="Picture of the poster"
             width={0}
             height={0}
@@ -49,16 +32,24 @@ export default async function Movie({ params }: { params: { id: string } }) {
         </div>
         <div className="flex flex-col sm:flex-[2]">
           <h1 className="font-bold text-2xl">
-            {movie.original_title}{" "}
+            {movie.title ?? movie.original_title}{" "}
             {
               <span className="font-normal text-base">
                 {movie.release_date.split("-")[0]}
               </span>
-            }
+            }{" "}
           </h1>
-          <h2 className="mb-4 text-lg px-2 py-1 bg-gray-200 w-fit rounded-md">
-            {Math.round(movie.vote_average * 10) / 10} / 10
-          </h2>
+          <div className="flex flex-row gap-2">
+            <h2 className="mb-4 text-lg px-2 py-1 bg-gray-200 w-fit rounded-md">
+              {Math.round(movie.vote_average * 10) / 10} / 10
+            </h2>
+            <Favourite
+              id={params.id}
+              isFav={isFav}
+              user={user}
+              bigText={true}
+            />
+          </div>
           <p className="mb-2">{movie.overview}</p>
           <div className="flex flex-row gap-2 flex-wrap my-2 mb-4">
             {movie.genres.map((genre) => {
@@ -72,34 +63,7 @@ export default async function Movie({ params }: { params: { id: string } }) {
               );
             })}
           </div>
-          <h3 className="font-bold text-lg mb-3">Similar movies</h3>
-          <div className="pl-3 flex flex-col gap-3 text-base">
-            {similarMovies.length !== 0
-              ? similarMovies.map((movie) => {
-                  return (
-                    <div key={movie.id} className="flex flex-col">
-                      <div className="flex flex-row">
-                        <Link
-                          href={`/movie/${movie.id}`}
-                          className="font-bold hover:underline"
-                        >
-                          {movie.title ? movie.title : movie.original_title}
-                        </Link>
-                        <div className="ml-2 text-sm px-2 bg-gray-200 w-fit rounded-md text-nowrap text-center leading-normal">
-                          {Math.round(movie.vote_average * 10) / 10} / 10
-                        </div>
-                      </div>
-                      <p>
-                        {shortenSummary(movie.overview, 100)}
-                        {"  ("}
-                        {movie.release_date.split("-")[0]}
-                        {")"}
-                      </p>
-                    </div>
-                  );
-                })
-              : "No similar movies found..."}
-          </div>
+          <SimilarMovies id={params.id} />
         </div>
       </div>
     </div>
