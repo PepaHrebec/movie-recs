d360eac734394d0abd57c5d16a45fd3aa7be02c2
Josef Hrebec <josefhrebec02@gmail.com>
2024-02-19 23:15:15 +0100

    Add search navigation action

---
+++ app/actions/navigation-actions.ts
@@ -0,0 +1,10 @@
+"use server";
+
+import { redirect } from "next/navigation";
+
+export async function navigate(address: string) {
+  if (address === "") {
+    return;
+  }
+  redirect(`./search/${address}`);
+}
